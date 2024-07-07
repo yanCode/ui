b133b109290794ea07ed8af29393fb7b672dbcf9
David Mraz <david@atheros.ai>
2024-07-07 14:23:42 +0200

    feat: signup

--- signup-04/script.js
+++ signup-04/script.js
@@ -1,9 +1,13 @@
-let password = document.getElementById("password"); 
-let power = document.getElementById("power-point"); 
 
-password.addEventListener('change', function () { 
-    let point = 0; 
-    let value = password.value; 
+const signupForm = document.getElementsByClassName("signup-form")[0];
+const email = document.getElementById("email");
+const password = document.getElementById("password"); 
+const confirmPassword = document.getElementById("confirm-password");
+const power = document.getElementById("power-point"); 
+
+const passwordStrength = function () { 
+    let point = 0;
+    let value = password.value;
     let widthPower =  
         ["1%", "25%", "50%", "75%", "100%"]; 
     let colorPower =  
@@ -15,10 +19,31 @@ password.addEventListener('change', function () {
         arrayTest.forEach((item) => { 
             if (item.test(value)) { 
                 point += 1; 
-            } 
+            }
         }); 
     }
 
     power.style.width = widthPower[point]; 
-    power.style.backgroundColor = colorPower[point]; 
+    power.style.backgroundColor = colorPower[point];
+};
+
+// inspired from geeks from geeks
+password.addEventListener('keyup', passwordStrength);
+
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // before submit
 });
+
+// check password strength
+function onChange() {
+  if (confirmPassword.value === password.value) {
+    confirmPassword.setCustomValidity("");
+  } else {
+    confirmPassword.setCustomValidity("Passwords do not match!");
+  }
+}
+
+password.addEventListener("change", onChange);
+confirmPassword.addEventListener("change", onChange);
+
